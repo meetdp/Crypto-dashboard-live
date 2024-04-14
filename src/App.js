50897b8bd124d35c3437f4d4ca3932008d3c3a60
meetdp <92285255+meetdp@users.noreply.github.com>
2024-04-14 17:29:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import CardSection from './components/CardSection';
+import ChartSection from './components/ChartSection';
+import Header from './components/Header';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Id: "bitcoin",
+      Data: {}
+    }
+  }
+  fetchData = async () => {
+    try {
+      let response = await fetch('https://api.coingecko.com/api/v3/coins/' + this.state.Id);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      let jsonData = await response.json();
+      this.setState({ Data: jsonData });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle the error appropriately, e.g., display an error message to the user
+    }
+  }
+  
+
+  handleSubmit = async (event)=>{
+    console.log(event.target.value)
+    await this.setState({Id: event.target.value, Data:this.state.Data})
+    this.fetchData()
+  }
+
+  componentDidMount() {
+    this.fetchData()
+    // this.interval = setInterval(() => this.fetchData(), 200);
+
+  }
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
+
+  render() {
+    return (
+      <div>
+        <Header handle_Submit = {this.handleSubmit} />
+        <CardSection coinName={this.state.Data.name} currentPrice={this.state.Data.market_data ? this.state.Data.market_data.current_price["usd"] : ""}
+          mCap24={this.state.Data.market_data ? this.state.Data.market_data.market_cap_change_percentage_24h : ""}
+          ath={this.state.Data.market_data ? this.state.Data.market_data.ath.usd : ""} atl={this.state.Data.market_data ? this.state.Data.market_data.ath.usd : ""}
+          sentiment={this.state.Data.sentiment_votes_up_percentage} high24={this.state.Data.market_data ? this.state.Data.market_data.high_24h["usd"] : ""}
+          low24={this.state.Data.market_data ? this.state.Data.market_data.low_24h["usd"] : ""} />
+        <ChartSection Id={this.state.Id} priceChange24={this.state.Data.market_data ? this.state.Data.market_data.price_change_24h_in_currency.usd : ""} 
+        MarketCap={this.state.Data.market_data ? this.state.Data.market_data.market_cap.usd  : ""}
+        TotVol={this.state.Data.market_data ? this.state.Data.market_data.total_volume.usd  : ""}
+        Circulating= {this.state.Data.market_data ? this.state.Data.market_data["circulating_supply"] : ""}
+        twitterF = {this.state.Data.community_data ? this.state.Data.community_data.twitter_followers : ""}
+        />
+      </div>
+    )
+  }
+}
